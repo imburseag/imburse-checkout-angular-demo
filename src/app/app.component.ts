4f368ddbd984c9c32ab0f7aa40a571ee9d5a443a
Daniel Baker <danpbaker90@gmail.com>
2021-07-09 10:48:37 +0100

    Added event listener examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZWxhdGlvbmlkIjoiMEhNOFVRRDFFUUVJUjowMDAwMDAwMiIsInRlbmFudGlkIjoiMzhmNmQ0OTctOWU0MS00M2Q1LTg3NTctYTRkNGI2ZWExYjUwIiwib3JkZXJyZWYiOiJEQU5fT1JERVIiLCJpbnN0cnVjdGlvbnJlZiI6IkRBTl9JTlNUUlVDVElPTl83MCIsImN1c3RvbWVycmVmIjoiREFOIiwianRpIjoiMWRlM2YxNGYtZDE2OS00ZmUyLTg4ZmUtZWNlYzkyMDliYWIyIiwicm9sZSI6InRlbmFudC10cmFuc2FjdGlvbi1yZWFkIiwiYmlsbGluZ2NpdHkiOiJDaXR5IiwiYmlsbGluZ3N0YXRlIjoiU3RhdGUiLCJiaWxsaW5nY291bnRyeSI6IkdCIiwiYmlsbGluZ3N0cmVldDEiOiIxIFRlc3QgU3RyZWV0IiwiYmlsbGluZ3N0cmVldDIiOiJBbm90aGVyIFN0cmVldCIsImJpbGxpbmdwb3N0Y29kZSI6IjEyMyBBQkMiLCJjdXN0b21lcmVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImNhcmRob2xkZXJuYW1lIjoiTVIgVCBFU1QiLCJuYmYiOjE2MjU3NTM0NTQsImV4cCI6MTYyNTc1OTUxNCwiaXNzIjoiaHR0cHM6Ly9jaS1vcGVuaWQuaW1idXJzZS5uZXQvIiwiYXVkIjoiaHR0cHM6Ly9jaS1hcGkuaW1idXJzZS5uZXQvIn0.A3rB9bT10bHtw-uFXIJtfN-JmwRv5DpnoydDVAOkf1xARuDTlu7CA2rcSw7KEq3hVazrXtaU0z1qZEvF-ozLISAfslvI3xV-PEdDViH1Wi9ux0g_EfkNrchPRES01TZEKXrSuFl3FZbdtMGrFtzRey4XRKE4gx7iRsFAugjP7ztnbVOWFKy4laNZllIj2g4KFEf71xXHWtiBcDpH12W1BKk19IPAWAPdnXZaeh7pKdcz4qhAHhr9kwHM-vr1KJ_ot87mS9WDsEYvriishmPNEnAoJD9cEu3Fqwuq2wLpZjSxKp1e3KoAhXXZMGhb0xBZ0XpiZRAoUV39oo-ULLNaVQ';
-  returnURL = 'http://localhost:4200'
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    const imburseCheckout = this.elementRef.nativeElement.querySelector('imburse-checkout');
+    const handleLoaded = () => {
+      console.log('imburse_checkout:component_loaded')
+    };
+
+    const handleSuccess = () => {
+      console.log('imburse_checkout:payment_success')
+    };
+
+    const handleFailure = () => {
+      console.log('imburse_checkout:payment_failed')
+    };
+
+    imburseCheckout?.addEventListener('imburse_checkout:component_loaded', handleLoaded);
+    imburseCheckout?.addEventListener('imburse_checkout:payment_success', handleSuccess);
+    imburseCheckout?.addEventListener('imburse_checkout:payment_failed', handleFailure);
+  }
+
+  token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZWxhdGlvbmlkIjoiMEhNOFVRRDFFUUVJUzowMDAwMDAwMiIsInRlbmFudGlkIjoiMzhmNmQ0OTctOWU0MS00M2Q1LTg3NTctYTRkNGI2ZWExYjUwIiwib3JkZXJyZWYiOiJEQU5fT1JERVIiLCJpbnN0cnVjdGlvbnJlZiI6IkRBTl9JTlNUUlVDVElPTl83MCIsImN1c3RvbWVycmVmIjoiREFOIiwianRpIjoiYTMyZmUzZGUtMWZhNS00MzJmLTg0ZGMtY2ZiZjQyOTU2MmQ4Iiwicm9sZSI6InRlbmFudC10cmFuc2FjdGlvbi1yZWFkIiwiYmlsbGluZ2NpdHkiOiJDaXR5IiwiYmlsbGluZ3N0YXRlIjoiU3RhdGUiLCJiaWxsaW5nY291bnRyeSI6IkdCIiwiYmlsbGluZ3N0cmVldDEiOiIxIFRlc3QgU3RyZWV0IiwiYmlsbGluZ3N0cmVldDIiOiJBbm90aGVyIFN0cmVldCIsImJpbGxpbmdwb3N0Y29kZSI6IjEyMyBBQkMiLCJjdXN0b21lcmVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImNhcmRob2xkZXJuYW1lIjoiTVIgVCBFU1QiLCJuYmYiOjE2MjU4MjE5NzYsImV4cCI6MTYyNTgyODAzNiwiaXNzIjoiaHR0cHM6Ly9jaS1vcGVuaWQuaW1idXJzZS5uZXQvIiwiYXVkIjoiaHR0cHM6Ly9jaS1hcGkuaW1idXJzZS5uZXQvIn0.WYTOrFflNCjs5sF1xqiBEKcX72ZlZM78W-1BELMTSEOUZ8yrPKuV7ARyyax_SJtoUWXNQhb0Bzywe8LTjKH9XYy-j7Wqcl3cbZaw01jnxfBxpghIlD8jCxJCkDUjqFS9caY1XiwJ8VywJlJHhsHcdQael8rIwW09MdDbd4M0Ou9_egtTwqp3ERITdzzSKP6_h9JVwOiAUWYEoKXALXt09Ti8cJyg-MgJvcIPnfrwYuEiC7tJG1xWyUZ2ePL6h3nCSXsFM5npkXpunitz2VoUOhUDAD7VXepNG82i_smIes7NU2cCsemIrGgq13RggnMchDk3MG4LSo99TSx-m-EvrQ';
+  returnURL = 'http://localhost:4200';
+  language = 'EN';
+  themeID = 'YOUR_THEME_ID';
 }
